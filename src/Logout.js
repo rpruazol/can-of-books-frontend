2e8f509cf01bea614341b62da9697289adbbca32
Monika <monidavies@gmail.com>
2023-02-16 18:26:27 -0800

    header css

--- src/Logout.js
+++ src/Logout.js
@@ -1,13 +1,14 @@
 import { useAuth0 } from "@auth0/auth0-react";
 import React from "react";
+import Button from 'react-bootstrap/Button';
 
 const LogoutButton = () => {
   const { logout } = useAuth0();
 
   return (
-    <button onClick={() => logout({ returnTo: window.location.origin })}>
+    <Button variant="test" onClick={() => logout({ returnTo: window.location.origin })}>
       Log Out
-    </button>
+    </Button>
   );
 };
 
